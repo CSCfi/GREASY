5b1431215ff5bb80f0c11072055502f5ae29b141
Victor Holanda <victorusu@gmail.com>
2019-03-07 21:39:50 +0100

    Add missing CSCS-related features
    Add [@ folder @] syntax
    Add workaround for GPU utilization
    Revert the greasy usage to
    ```
    greasy task.file
    ```
    from
    ```
    greasy -f task.file -n <number-of-mpi-ranks-per-task>
    ```
    
    Now one needs to set
    ```
    export GREASY_NWORKERS_PER_NODE=X
    ```
    or
    ```
    export GREASY_NWORKERS=Y
    ```
    and automagically the remainder variables will be computed
    ```
    export GREASY_NTASKS_PER_WORKER=A
    export GREASY_NODELIST=B
    ```
    
    The variable `GREASY_NTASKS_PER_WORKER` replaces the need for the `-n`
    flag. And the `GREASY_NODELIST` variable should not be set anymore.
    This allows for a simpler and unique interface to define the distributed
    (MPI) and shared (OpenMP) tasks.
    
    For instance, if one wants to use distributed tasks (MPI) enabled tasks,
    where each task will make use of 6 MPI ranks and no OpeMP thread, one
    can
    ```
    
    export CRAY_CUDA_MPS=1
    export CUDA_VISIBLE_DEVICES=0
    export GPU_DEVICE_ORDINAL=0
    
    export GREASY_NWORKERS_PER_NODE=2
    
    greasy tasks.txt
    ```
    
    However, if one wants to use non distributed tasks (OpenMP), allow for
    up to 6 OpenMP thread, one can use
    ```
    
    export CRAY_CUDA_MPS=1
    export CUDA_VISIBLE_DEVICES=0
    export GPU_DEVICE_ORDINAL=0
    
    export GREASY_NWORKERS_PER_NODE=2
    
    greasy tasks.txt
    ```
    
    Furthermore, if one needs to use distributed and shared tasks
    (MPI+OpenMP), then one can set
    ```
    
    export CRAY_CUDA_MPS=1
    export CUDA_VISIBLE_DEVICES=0
    export GPU_DEVICE_ORDINAL=0
    
    export GREASY_NWORKERS_PER_NODE=2
    
    greasy tasks.txt
    ```

--- src/basicengine.h
+++ src/basicengine.h
@@ -1,17 +1,17 @@
-/* 
+/*
  * This file is part of GREASY software package
  * Copyright (C) by the BSC-Support Team, see www.bsc.es
- * 
+ *
  * GREASY is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
- * 
+ *
  * GREASY is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with GREASY. If not, see <http://www.gnu.org/licenses/>.
  *
@@ -34,20 +34,20 @@
   */
 class BasicEngine : public AbstractSchedulerEngine
 {
-  
+
 public:
 
   /**
    * Constructor that adds the filename to process.
    * @param filename path to the task file.
    */
-  BasicEngine (const string& filename ); 
-  
+  BasicEngine (const string& filename, int ntasks_per_worker);
+
   /**
    * Perform the initialization of the engine and the MPI environment.
    */
   virtual void init();
-  
+
   /**
    * Execute the engine. It is divided into 2 different parts, for master and workers.
    */
@@ -55,46 +55,50 @@ public:
 
 
 protected:
-  
+
   /**
    * Allocate a task in a free worker, sending the command to it.
    * @param task A pointer to a GreasyTask object to allocate.
    */
   virtual void allocate(GreasyTask* task);
-  
+
   /**
    * Wait for any worker to complete their tasks and retrieve
    * results.
    */
   virtual void waitForAnyWorker();
-  
+
   /**
    * Run the command corresponding to a task.
    * @param task The task to be executed.
    * @param worker The index of the worker in charge.
    * @return The exit status of the command.
-   */  
+   */
   virtual int executeTask(GreasyTask *task, int worker);
-  
+
   /**
    * Checks if a given node is the local node.
    * @param task The node to be checked.
    * @return True if local node, false otherwise.
-   */  
+   */
   bool isLocalNode(string node);
 
   /**
    * Get the name of the node where the worker is asigned.
    * @param task The worker id.
    * @return the string containing the node name.
-   */  
+   */
   string getWorkerNode(int worker);
-  
+
   map<pid_t,int> pidToWorker; /**<  Map to translate a pid to the corresponding worker. */
   map<int, GreasyTimer> workerTimers; /**<  Map of worker timers to know elapsed time of tasks. */
   map<int, string> workerNodes; /**<  Map of worker nodes to know in which node to send each worker's tasks. */
   string masterHostname; ///< String to hold the master hostname.
   bool remote; ///< Flag to know whether the engine will need to do remote tasks.
+
+private:
+  bool gpu_nodes;
+
 };
 
 #endif // BASICENGINE_H
